c41cb8ffa36b680697c7fd54bb9c89b74e02adc8
Chris LoJacono <clojacono92@gmail.com>
2021-04-13 19:05:12 -0500

    new seed

--- ToboggonApp/Toboggon/Controllers/UserController.cs
+++ ToboggonApp/Toboggon/Controllers/UserController.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Toboggon.Controllers
+namespace Toboggan.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
