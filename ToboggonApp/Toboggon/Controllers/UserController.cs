f07c5d80866724208dad0c75104f3277d25c472f
Chris LoJacono <clojacono92@gmail.com>
2021-04-14 18:50:03 -0500

    get all users works

--- ToboggonApp/Toboggon/Controllers/UserController.cs
+++ ToboggonApp/Toboggon/Controllers/UserController.cs
@@ -4,12 +4,25 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Toboggan.DataAccess;
 
 namespace Toboggan.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/Users")]
     [ApiController]
     public class UserController : ControllerBase
     {
+        UserRepository _repo;
+
+        public UserController()
+        {
+            _repo = new UserRepository();
+        }
+
+        [HttpGet]
+        public IActionResult GetAllUsers()
+        {
+            return Ok(_repo.GetAll());
+        }
     }
 }
--- ToboggonApp/Toboggon/DataAccess/UserRepository.cs
+++ ToboggonApp/Toboggon/DataAccess/UserRepository.cs
@@ -2,10 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using Dapper;
+using Toboggan.Models;
 
 namespace Toboggan.DataAccess
 {
     public class UserRepository
     {
+        const string ConnectionString = "Server=localhost;Database=Toboggan;Trusted_Connection=True";
+
+        public List<User> GetAll()
+        {
+            using var connection = new SqlConnection(ConnectionString);
+
+            var sql = @"select *
+                        from [User]";
+            var results = connection.Query<User>(sql).ToList();
+
+            return results;
+
+        }
+
     }
 }
