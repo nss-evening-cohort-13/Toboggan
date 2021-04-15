5592b894066c40f354f6a54df78f25639de2c473
Chris Calhoun <ctcalhoun2@gmail.com>
2021-04-14 19:00:52 -0500

    created PaymentTypeRepository file.

--- ToboggonApp/Toboggon/Controllers/PaymentTypeController.cs
+++ ToboggonApp/Toboggon/Controllers/PaymentTypeController.cs
@@ -1,11 +1,15 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Toboggan.Controllers
 {
-    public class PaymentTypeController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentTypeController : ControllerBase
     {
     }
 }
