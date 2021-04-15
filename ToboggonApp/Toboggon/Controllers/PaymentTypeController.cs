de28b2de698da193cd3fa7befa8c0bd20e1146f6
Chris Calhoun <ctcalhoun2@gmail.com>
2021-04-14 19:34:13 -0500

    completed paymentType controller.

--- ToboggonApp/Toboggon/Controllers/PaymentTypeController.cs
+++ ToboggonApp/Toboggon/Controllers/PaymentTypeController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Toboggan.DataAccess;
+using Toboggan.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -7,9 +9,35 @@ using System.Threading.Tasks;
 
 namespace Toboggan.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/PaymentType")]
     [ApiController]
     public class PaymentTypeController : ControllerBase
     {
+        PaymentTypeRepository _repo;
+        public PaymentTypeController()
+        {
+            _repo = new PaymentTypeRepository();
+        }
+
+        //GET to /api/PaymentType
+        [HttpGet]
+        public IActionResult GetAllPaymentTypes()
+        {
+            return Ok(_repo.GetAll());
+        }
+
+        //GET to /api/PaymentType/{id}
+        [HttpGet("{id")]
+        public IActionResult GetById(int id)
+        {
+            var paymentType = _repo.Get(id);
+
+            if (paymentType == null)
+            {
+                return NotFound("This PaymentType id does not exist");
+            }
+
+            return Ok(paymentType);
+        }
     }
 }
