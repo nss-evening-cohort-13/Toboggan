04864299806f55eb03d379c765e463115d8a79f0
Chris Calhoun <ctcalhoun2@gmail.com>
2021-04-14 20:09:12 -0500

    completed order line item repo.

--- ToboggonApp/Toboggon/DataAccess/OrderLineItemRepository.cs
+++ ToboggonApp/Toboggon/DataAccess/OrderLineItemRepository.cs
@@ -2,10 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Toboggan.Models;
+using Microsoft.Data.SqlClient;
+using Dapper;
 
 namespace Toboggan.DataAccess
 {
     public class OrderLineItemRepository
     {
+        const string ConnectionString = "Server=localhost;Database=LoafAndStranger;Trusted_Connection=True;";
+        
+        public List<OrderLineItem> GetAll()
+        {
+            using var db = new SqlConnection(ConnectionString);
+
+            var sql = @"Select *
+                        From OrderLineItem";
+            return db.Query<OrderLineItem>(sql).ToList();
+        }
+
+        public OrderLineItem Get(int id)
+        {
+            var sql = @"Select *
+                        From OrderLineItem
+                        where Id = @id";
+
+            using var db = new SqlConnection(ConnectionString);
+
+            var orderLineItem = db.QueryFirstOrDefault<OrderLineItem>(sql, new { id = id });
+            return orderLineItem;
+        }
     }
 }
