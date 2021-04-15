e47234affb13b72d1ecc496e8e2989e7e58c8fdb
Chris Calhoun <ctcalhoun2@gmail.com>
2021-04-14 19:07:07 -0500

    completed payment type model.

--- ToboggonApp/Toboggon/Models/PaymentType.cs
+++ ToboggonApp/Toboggon/Models/PaymentType.cs
@@ -5,7 +5,20 @@ using System.Threading.Tasks;
 
 namespace Toboggan.Models
 {
+    //Models are for storing pieces of information, not for having behavior
     public class PaymentType
     {
+        public int Id { get; set; }
+        public int AccountNumber { get; set; }
+        public PaymentTypeName Name { get; set; }
     }
+
+    public enum PaymentTypeName
+    {
+        MasterCard,
+        Visa,
+        PayPal,
+        Discover
+    }
+
 }
