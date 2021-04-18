6ce6b3f7be5d4b1c25c321e6e54ab602f82167e3
Chris Calhoun <ctcalhoun2@gmail.com>
2021-04-17 23:15:53 -0500

    completed post for order function for order line item.

--- ToboggonApp/Toboggon/Models/OrderLineItem.cs
+++ ToboggonApp/Toboggon/Models/OrderLineItem.cs
@@ -10,5 +10,6 @@ namespace Toboggan.Models
         public int Id { get; set; }
         public int ProductId { get; set; }
         public int Quantity { get; set; }
+        public int OrderId { get; set; }
     }
 }
