4e174e21f757162e54adfa56d2b3c8e054edce68
Liz <liz@Liz-s-Mac.local>
2021-04-17 22:51:33 -0500

    setup file structure, create react app

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -4,10 +4,7 @@ import './App.scss';
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <h2>INSIDE APP COMPONENT</h2>
-        <button className="btn btn-info">I am a button</button>
-      </div>
+      <div className="App"></div>
     );
   }
 }
