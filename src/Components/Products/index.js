d647866821fd3ad2eb7febad7601739437797c75
Chris LoJacono <clojacono92@gmail.com>
2021-04-20 18:50:32 -0500

    final mock seed

--- src/Components/Products/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Products() {
-  return (
-    <div className="Products">
-      <h2>Products Page</h2>
-    </div>
-  );
-}
--- src/Components/UserProfile/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function UserProfile() {
-  return (
-    <div className="User-Profile">
-      <h2>User Profile Page</h2>
-    </div>
-  );
-}
