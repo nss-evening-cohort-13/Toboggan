3063bcdccadd97b5e044c37920b9b8b4e953f50c
Liz Barnes <embarnes95@gmail.com>
2021-04-21 14:11:01 -0700

    start building out getAllShops functions

--- TobogganApp.ui/src/App/App.js
+++ TobogganApp.ui/src/App/App.js
@@ -3,11 +3,13 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import Routes from '../helpers/Routes';
 import Nav from '../Components/Nav';
 import './App.scss';
+import { GetAllShops } from '../helpers/data/shopData';
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
+      <div className='App'>
+        <GetAllShops />
         <Router>
           <Nav />
           <Routes />
--- TobogganApp.ui/src/helpers/data/shopData.js
+++ TobogganApp.ui/src/helpers/data/shopData.js
@@ -1,1 +1,12 @@
-import { baseUrl } from "./config.json";
+import axios from 'axios';
+import { baseUrl } from './config.json';
+
+const shopUrl = `${baseUrl}/Shop`;
+
+const GetAllShops = () => {
+    return new Promise((resolve, reject) =>
+        axios.get(shopUrl).then(response => resolve(response.data))
+    );
+}
+
+export default {GetAllShops};
