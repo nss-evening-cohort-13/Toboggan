b19492eba5ea8fa9a41c30f5d2de694b3fac07c7
Chris LoJacono <clojacono92@gmail.com>
2021-04-20 20:38:19 -0500

    small change to eslint

--- TobogganApp.ui/src/Views/HomeView.js
+++ TobogganApp.ui/src/Views/HomeView.js
@@ -2,18 +2,25 @@ import React, { Component } from 'react';
 import ProductCard from '../Components/Products/ProductCard';
 import { getAllProducts } from '../helpers/data/productData';
 
+export default class HomePageView extends Component {
+  state = {
+    products: [],
+  };
 
-export default class HomePageView extends Component{
-    state = {
-        products: [],
-    }
+  componentDidMount() {
+    getAllProducts().then((response) => {
+      this.setState({
+        products: response,
+      });
+    });
+  }
 
-    componentDidMount() {
-        getAllProducts().then((response) => {
-            this.setState({
-                products: response,
-            })
-        })
-    }
-    
+  render() {
+    const { products } = this.state;
+    const renderProducts = () =>
+      products.map((product) => (
+        <ProductCard key={product.id} productData={product} />
+      ));
+      
+  }
 }
--- TobogganApp.ui/src/helpers/data/productData.js
+++ TobogganApp.ui/src/helpers/data/productData.js
@@ -1,15 +1,15 @@
 import axios from 'axios';
-import { baseUrl } from "./config.json";
+import { baseUrl } from './config.json';
 
 const productsUrl = `${baseUrl}/Products`;
 
 const getAllProducts = () => new Promise((resolve, reject) => {
-    axios.get(productsUrl).then((response) => {
-      resolve(response.data);
-    }).catch((error) => reject(error));
+    axios
+      .get(productsUrl)
+      .then((response) => {
+        resolve(response.data);
+      })
+      .catch((error) => reject(error));
   });
 
-
-  export {
-    getAllProducts
-  };
+export { getAllProducts };
