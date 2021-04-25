40411ee692afb6df4692e049e714795376aaa379
Chris Calhoun <ctcalhoun2@gmail.com>
2021-04-25 11:01:05 -0500

    created model for CategoryThreeNewestProducts.

---
+++ ToboggonApp/Toboggon/Models/CategoryThreeNewestProducts.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Toboggan.Models
+{
+    public class CategoryThreeNewestProducts
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public double Price { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string ProductImage { get; set; }
+
+    }
+}
