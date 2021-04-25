57181aee280e82ee4dbb859762d3479f0ba1595d
Chris Calhoun <ctcalhoun2@gmail.com>
2021-04-25 16:07:25 -0500

    got three newest product cards to show up for each category.

--- TobogganApp.ui/src/Views/ProductCategoryView.js
+++ TobogganApp.ui/src/Views/ProductCategoryView.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import categoryData from '../helpers/data/categoryData';
+import ProductCard from '../Components/Card/ProductCard';
 
 export default class ProductCategoryView extends Component {
   state = {
@@ -8,17 +9,22 @@ export default class ProductCategoryView extends Component {
 
   componentDidMount() {
     categoryData
-      .GetQuantityOfProductsPerCategory()
+      .GetCategoryProducts()
       .then((response) => this.setState({ categories: response }));
   }
 
   render() {
     const { categories } = this.state;
+
+    const renderProducts = (category) => category.products.slice(0, 3).map((product) => (<ProductCard key={product.Id} productData={product} />));
+
     const renderCategories = () => categories.map((category) => (
     <>
       <h3 key={category.id}>{category.name} - {category.quantity}</h3>
       {/* Show last 3 created products from each category */}
-      <p>product here</p>
+      <div className="d-flex flex-wrap justify-content-center">
+        {renderProducts(category)}
+      </div>
     </>
     ));
 
