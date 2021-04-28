9311b2356387fe20b31e17bd66339fe7f36bb067
Liz Barnes <embarnes95@gmail.com>
2021-04-27 18:28:55 -0700

    comment out previous function that printed user and their shops, now printing all shops in grid

--- TobogganApp.ui/src/Views/ShopView.js
+++ TobogganApp.ui/src/Views/ShopView.js
@@ -5,11 +5,13 @@ import ShopCard from '../Components/Card/ShopCard';
 
 class Shops extends React.Component {
   state = {
+    shops: [],
     users: [],
     loading: true,
   };
 
   componentDidMount() {
+    this.getAllOfTheShops();
     this.getAllOfTheUsers();
   }
 
@@ -17,7 +19,7 @@ class Shops extends React.Component {
     shopData.getAllShops().then((response) => {
       this.setState({
         shops: response,
-      });
+      }, this.setLoading);
     });
   }
 
@@ -25,7 +27,7 @@ class Shops extends React.Component {
     userData.getAllUsers().then((response) => {
       this.setState({
         users: response,
-      }, this.setLoading);
+      });
     });
   }
 
@@ -39,40 +41,44 @@ class Shops extends React.Component {
     clearInterval(this.timer);
   }
 
-    printUserShops = () => {
-      const { users } = this.state;
+  // printUserShops = () => {
+  //   const { users } = this.state;
 
-      const results = users.map((user) => {
-        if (user.shops.length) {
-          const html = <div className='m-5'>
-            <h1>{user.firstName}</h1>
-            <div className='d-flex justify-content-center'>
-              {user.shops.map((shop) => <ShopCard key={shop.id} shopData={shop} />)}
-            </div>
-          </div>;
-          return html;
-        }
-        return [(<></>)];
-      });
+  //   const results = users.map((user) => {
+  //     if (user.shops.length) {
+  //       const html = <div className='m-5'>
+  //         <h1>{user.firstName}</h1>
+  //         <div className='d-flex justify-content-center'>
+  //           {user.shops.map((shop) => <ShopCard key={shop.id} shopData={shop} />)}
+  //         </div>
+  //       </div>;
+  //       return html;
+  //     }
+  //     return [(<></>)];
+  //   });
 
-      return results;
-    }
+  //   return results;
+  // }
 
-    render() {
-      const { loading } = this.state;
+  render() {
+    const { shops, loading } = this.state;
+    const renderShops = () => shops.slice(0, 20).map((shop) => (<ShopCard key={shop.Id} shopData={shop} />));
 
-      return (
-        <>
-          {loading ? (
-            <h1>Loading</h1>
-          ) : (
-              <div id='shops-container'>
-                {this.printUserShops()}
-              </div>
-          )}
-        </>
-      );
-    }
+    return (
+      <>
+        {loading ? (
+          <h1>Loading</h1>
+        ) : (
+          <div id='shops-container' className='d-flex flex-wrap justify-content-center'>
+            {renderShops()}
+          </div>
+          //   <div id='shops-container'>
+          //     {this.printUserShops()}
+          //   </div>
+        )}
+      </>
+    );
+  }
 }
 
 export default Shops;
