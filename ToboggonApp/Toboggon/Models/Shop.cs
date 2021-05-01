2ecf01721e578a7a1e3aea30e86efb675d6c8b51
Chris LoJacono <clojacono92@gmail.com>
2021-05-01 09:44:35 -0500

    description added to the shop class

--- ToboggonApp/Toboggon/Models/Shop.cs
+++ ToboggonApp/Toboggon/Models/Shop.cs
@@ -12,5 +12,6 @@ namespace Toboggan.Models
         public int UserId { get; set; }
         public DateTime CreatedDate { get; set; }
         public string ShopImage { get; set; }
+        public string Description { get; set; }
     }
 }
