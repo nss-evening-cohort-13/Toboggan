dd7f0ff2b42b2833dca7c4c40b1a04b4b4db7293
Chris Calhoun <ctcalhoun2@gmail.com>
2021-05-03 17:00:53 -0500

    removed unnecessary functions.

--- TobogganApp.ui/src/helpers/data/userData.js
+++ TobogganApp.ui/src/helpers/data/userData.js
@@ -9,29 +9,4 @@ const getSingleUser = (id) => new Promise((resolve, reject) => {
   }).catch((error) => reject(error));
 });
 
-const checkIfUserExistsInDB = (user) => {
-  axios.get(`${userDataUrl}/${user.Id}`).then((response) => {
-    if (Object.values(response.data).length === 0) {
-      axios.post(`${userDataUrl}`, user);
-      console.warn('user posted');
-    } else {
-      console.warn('User already exists.');
-    }
-  }).catch((error) => console.warn(error));
-};
-
-const setCurrentUser = (userObj) => {
-  const user = {
-    Id: userObj.uid,
-  };
-
-  console.warn(userObj);
-  // const loggedIn = window.sessionStorage.getItem('token');
-  // if (loggedIn) {
-  //   console.warn('hit this point');
-  checkIfUserExistsInDB(user);
-  // }
-  return user;
-};
-
-export default { getSingleUser, setCurrentUser };
+export default { getSingleUser };
