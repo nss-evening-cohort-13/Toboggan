908fefceea23a78e898b66436db20c3daf27dccb
Liz Barnes <embarnes95@gmail.com>
2021-05-04 17:21:23 -0700

    add product and seller heading, removed unused code, refactored showResults functions

--- TobogganApp.ui/src/Views/SearchProductsResults.js
+++ TobogganApp.ui/src/Views/SearchProductsResults.js
@@ -7,35 +7,13 @@ import UserCard from '../Components/Card/SellerCard';
 
 export default class SearchProductResults extends Component {
   state = {
-    loading: true,
     productResults: [],
     sellerResults: [],
-    sellers: [],
     searchInput: '',
   };
 
   componentWillMount() {
-    this.getSellers();
     this.showResults();
-  }
-
-  getSellers = () => {
-    userData.getSellerData().then((response) => {
-      console.warn('user resp', response);
-      this.setState({
-        sellers: response,
-      });
-    });
-  }
-
-  setLoading = () => {
-    this.timer = setInterval(() => {
-      this.setState({ loading: false });
-    }, 1000);
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.timer);
   }
 
   showResults = () => {
@@ -44,7 +22,6 @@ export default class SearchProductResults extends Component {
     productData
       .getFilteredProducts(searchInput.toLowerCase())
       .then((response) => {
-        console.warn('product resp', response);
         this.setState({
           productResults: response,
           searchInput,
@@ -54,39 +31,15 @@ export default class SearchProductResults extends Component {
     userData
       .getFilteredSellers(searchInput.toLowerCase())
       .then((response) => {
-        console.warn('seller in show results resp', response);
         this.setState({
           sellerResults: response,
         });
       });
   };
 
-  showSellerResults = () => {
-    const searchInput = this.props.match.params.term;
-    console.warn('search input', searchInput);
-
-    const { sellers } = this.state;
-
-    console.warn('sellers state', sellers);
-    const sellerResults = [];
-
-    const firstNameFilter = sellers.filter((seller) => seller.firstName.toLowerCase().includes(searchInput.toLowerCase()));
-    // const lastNameFilter = sellers.filter((seller) => seller.lastName.toLowerCase().includes(searchInput.toLowerCase()));
-
-    sellerResults.push(firstNameFilter);
-    // sellerResults.push(lastNameFilter);
-
-    this.setState({
-      sellerResults: firstNameFilter,
-    }, this.setLoading);
-
-    console.warn('seller state', sellerResults);
-  }
-
   componentDidUpdate(prevState) {
     if (prevState.match.params.term !== this.props.match.params.term) {
       this.showResults();
-      this.showSellerResults();
     }
   }
 
@@ -100,17 +53,24 @@ export default class SearchProductResults extends Component {
     return (
       <>
         <SearchBar />
-        <div className='d-flex flex-wrap justify-content-center'>
-          {showResults()}
-          {renderSellers()}
-        </div>
-        {/* {loading ? (
-          <h1>Loading</h1>
-        ) : (
-          <div>
-            {renderSellers()}
+        {productResults.length ? (
+          <div className='product-results-container'>
+            <h1>Products</h1>
+            <div className='d-flex flex-wrap justify-content-center'>
+              {showResults()}
+            </div>
           </div>
-        )} */}
+        ) : ''
+        }
+        {sellerResults.length ? (
+          <div className='seller-results-container'>
+            <h1>Sellers</h1>
+            <div className='d-flex flex-wrap justify-content-center'>
+              {renderSellers()}
+            </div>
+          </div>
+        ) : ''
+        }
       </>
     );
   }
