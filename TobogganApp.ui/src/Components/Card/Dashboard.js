297068707f04a3b9513d7f01de3f7f817be46160
Chris LoJacono <clojacono92@gmail.com>
2021-05-07 10:05:33 -0500

    hooked up but not sure if working properly

--- TobogganApp.ui/src/Components/Card/Dashboard.js
+++ TobogganApp.ui/src/Components/Card/Dashboard.js
@@ -12,12 +12,12 @@ export default function Dashboard(props) {
     <div className='d-flex User-Profile'>
     <div className='flex-column'>
     <h2>Total sales:</h2>
-    <div>${ totalInfo && totalInfo.length > 0 ? totalInfo[0].total : ''}</div>
+    <div>${totalInfo.length > 0 ? totalInfo[0].total : ''}</div>
     {console.warn(salesThisMonth)}
     <h2>Total sales for { salesThisMonth && salesThisMonth.length ? `${salesThisMonth[0].month}/${salesThisMonth[0].year}` : ''}:</h2>
     <div>{ salesThisMonth && salesThisMonth.length ? salesThisMonth[0].total : ''}</div>
     <h2>Average Per Item</h2>
-    <div> { totalInfo && totalInfo.length > 0 ? (totalInfo[0].total / totalInfo[0].totQuantity).toFixed(2) : '' } </div>
+    <div> { totalInfo.length !== NaN ? (totalInfo[0].total / totalInfo[0].totQuantity).toFixed(2) : '' } </div>
     <h2> Total Inventory by Category--</h2>
     <div>{categoryData && categoryData.length && categoryData.map((catData) => <CategoryInventory catData={catData}/>)}
     <h2>Total Sales by Category:</h2>
--- TobogganApp.ui/src/Views/UserDashboardViews/ShopDashboardView.js
+++ TobogganApp.ui/src/Views/UserDashboardViews/ShopDashboardView.js
@@ -1,11 +1,76 @@
 import React, { Component } from 'react';
+import BioCard from '../../Components/Card/UserBioCard';
+import userData from '../../helpers/data/userData';
+import orderData from '../../helpers/data/orderData';
+import catData from '../../helpers/data/categoryData';
+import Dashboard from '../../Components/Card/Dashboard';
 
-export default class ShopDashboardView extends Component {
+export default class Shop extends Component {
+  state = {
+    userInfo: '',
+    totalInfo: '',
+    totalByDate: '',
+    categoryDate: '',
+    toBeShipped: '',
+    orderLineItems: [],
+    error: '',
+    userId: this.props.user?.uid,
+  }
+
+  componentDidMount() {
+    userData.getSingleUser(this.state.userId)
+      .then((response) => {
+        this.setState({
+          userInfo: response,
+        });
+      });
+    orderData.fetchTotalAllSalesAvgById(this.state.userId)
+      .then((response) => {
+        this.setState({
+          totalInfo: response,
+        });
+      });
+    orderData.fetchTotalSalesByDate(this.state.userId)
+      .then((response) => {
+        this.setState({
+          totalByDate: response,
+        });
+      });
+    catData.GetCategoryData(this.state.userId)
+      .then((response) => {
+        this.setState({
+          categoryData: response,
+        });
+      });
+    orderData.fetchOrdersToBeShipped(this.state.userId)
+      .then((response) => {
+        this.setState({
+          toBeShipped: response,
+        });
+      });
+  }
+
+  getDate() {
+    return new Date();
+  }
+
+  salesThisMonth = () => this.state.totalByDate.filter((tot) => (this.getDate().getFullYear() === tot.year) && ((this.getDate().getMonth() + 1) == tot.month));
+
   render() {
+    const {
+      userInfo,
+      totalInfo,
+      categoryData,
+      toBeShipped,
+    } = this.state;
     return (
-      <div>
-        <h2>Shop Dashboard</h2>
+      <>
+      <h2>Shop Dashboard</h2>
+      <div className='d-flex User-Profile'>
+        <BioCard userData={ userInfo }/>
+        <Dashboard totalInfo={ totalInfo } salesThisMonth={this.salesThisMonth} categoryData={categoryData} toBeShipped={toBeShipped} />
       </div>
+      </>
     );
   }
 }
