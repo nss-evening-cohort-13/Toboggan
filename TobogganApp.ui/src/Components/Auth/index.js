335ffa4f55ae38f2ce48d2287d7ea1f728848658
Chris Calhoun <ctcalhoun2@gmail.com>
2021-05-10 14:38:23 -0500

    quick save.

--- TobogganApp.ui/src/Components/Auth/index.js
+++ TobogganApp.ui/src/Components/Auth/index.js
@@ -1,6 +1,4 @@
 import { Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
 import React, { Component } from 'react';
 import firebase from 'firebase/app';
 import {
--- TobogganApp.ui/src/Views/UserDashboardViews/UserDashboardView.js
+++ TobogganApp.ui/src/Views/UserDashboardViews/UserDashboardView.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Button from '@material-ui/core/Button';
 import Box from '@material-ui/core/Box';
 import { Link } from 'react-router-dom';
 
