b3ca0a9c08ff714e7a53e4174d643c15f227191c
wkotheimer <wkotheimer@gmail.com>
2021-05-09 22:01:41 -0500

    Add to cart button

---
+++ TobogganApp.ui/src/helpers/data/cartData.js
@@ -0,0 +1,38 @@
+import LocalStorage from '../localStorage';
+
+let cart = [];
+
+const loadCart = () => {
+  if (LocalStorage.getItem('cart')) {
+    cart = LocalStorage.getItem('cart');
+  } else {
+    cart = [];
+  }
+};
+
+const setCart = (item) => {
+  cart.push(item);
+  LocalStorage.setItem('cart', cart);
+};
+
+const getCart = () => cart;
+const emptyCart = () => {
+  cart.length = 0;
+
+  LocalStorage.removeItem('cart');
+};
+
+const removeItems = (e) => {
+  const itemTitle = e.target.closest('tr').id;
+  const filteredItems = cart.filter((item) => item.title !== itemTitle);
+  cart = filteredItems;
+
+  LocalStorage.setItem('cart', cart);
+};
+
+export default {
+  loadCart,
+  setCart,
+  getCart,
+  emptyCart,
+};
