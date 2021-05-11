bad30b73b42a7ac1626ef39225a1bc3d4bf0a11b
Liz Barnes <embarnes95@gmail.com>
2021-05-11 16:56:22 -0700

    create purchase history using material core ui Grid

--- TobogganApp.ui/src/Components/Card/PurchaseHistoryCard.js
+++ TobogganApp.ui/src/Components/Card/PurchaseHistoryCard.js
@@ -1,9 +1,67 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import ButtonBase from '@material-ui/core/ButtonBase';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    textAlign: 'left',
+    margin: theme.spacing(5),
+  },
+  paper: {
+    padding: theme.spacing(5),
+    margin: 'auto',
+    maxWidth: 850,
+  },
+  image: {
+    width: 250,
+    height: 250,
+  },
+  img: {
+    margin: 'auto',
+    display: 'block',
+    maxWidth: '100%',
+    maxHeight: '100%',
+  },
+}));
 
 export default function PurchaseHistoryCard({ orderData }) {
+  const classes = useStyles();
+
   return (
-    <div>
-      <h1>{orderData.Title}</h1>
+    <div className={classes.root}>
+      <Paper className={classes.paper}>
+        <Grid container spacing={2}>
+          <Grid item>
+            <ButtonBase className={classes.image}>
+              <img className={classes.img} alt="complex" src={orderData.ProductImage} />
+            </ButtonBase>
+          </Grid>
+          <Grid item xs={12} sm container>
+            <Grid item xs container direction="column" spacing={2}>
+              <Grid item xs>
+                <Typography gutterBottom variant="h4">
+                  {orderData.Title}
+                </Typography>
+                <Typography variant="h5" color="textSecondary" gutterBottom>
+                  {orderData.Description}
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Typography variant="h6" style={{ cursor: 'pointer' }}>
+                  Seller:
+                </Typography>
+              </Grid>
+            </Grid>
+            <Grid item>
+              <Typography variant="h5" color="textSecondary">${orderData.Price}</Typography>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Paper>
     </div>
   );
 }
