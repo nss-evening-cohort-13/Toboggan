8768c936e89810fe1ce33c0ea42d4fa7c1b1c6b9
Chris LoJacono <clojacono92@gmail.com>
2021-05-10 20:08:29 -0500

    initial

--- TobogganApp.ui/src/Views/UserDashboardViews/AccountInfoView.js
+++ TobogganApp.ui/src/Views/UserDashboardViews/AccountInfoView.js
@@ -3,9 +3,11 @@ import React, { Component } from 'react';
 export default class AccountInfoView extends Component {
   render() {
     return (
+      <>
       <div>
         <h2>Account Info</h2>
       </div>
+      </>
     );
   }
 }
