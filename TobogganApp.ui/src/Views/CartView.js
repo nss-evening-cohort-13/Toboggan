8cd45d4c3cb1853a927bb58c051b7a5e920486e2
Chris LoJacono <clojacono92@gmail.com>
2021-05-12 19:43:43 -0500

    card

--- TobogganApp.ui/src/Views/CartView.js
+++ TobogganApp.ui/src/Views/CartView.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import CartData from '../helpers/data/cartData';
+import ShoppingCartCard from '../Components/Card/SHoppingCartCard';
 
 export default class CartView extends Component {
   state = {
@@ -7,11 +8,11 @@ export default class CartView extends Component {
   };
 
   componentDidMount() {
+    const { products } = this.state;
     this.getCartProducts();
   }
 
   getCartProducts = () => {
-    // console.warn(cart);
     this.setState({
       products: CartData.getCart(),
     });
@@ -19,19 +20,20 @@ export default class CartView extends Component {
 
   render() {
     const { products } = this.state;
-
     let renderProducts;
     if (products && Object.keys(products) !== 0) {
       renderProducts = () => products.map((product) => (
-        <h2>{product}</h2>
-        // <h2 key={product.id}>{product.price}</h2>
+          <ShoppingCartCard productData={product}/>
+
       ));
     }
     return (
-      <div>
-        CartView
+      <>
+      <h1>Your Cart</h1>
+      <div className="d-flex flex-column justify-content-center">
         {renderProducts()}
       </div>
+      </>
     );
   }
 }
