f8ddb3321a3a8fecc0f6ccbf1fa30562c9319a28
Chris Calhoun <ctcalhoun2@gmail.com>
2021-05-15 10:47:41 -0500

    removed commented out functions in cartData.

--- TobogganApp.ui/src/helpers/data/cartData.js
+++ TobogganApp.ui/src/helpers/data/cartData.js
@@ -23,27 +23,9 @@ const emptyCart = () => {
   LocalStorage.removeItem('cart');
 };
 
-// const removeItems = (e) => {
-//   const itemTitle = e.target.closest('tr').id;
-//   const filteredItems = cart.filter((item) => item.title !== itemTitle);
-//   cart = filteredItems;
-
-//   LocalStorage.setItem('cart', cart);
-// };
-
-// const removeItem = (product) => {
-//   // console.warn('remove this product: ', product.id);
-//   let tempCart = LocalStorage.getItem('cart');
-//   // console.warn('tempcart', tempCart);
-//   // console.warn('tempcart-item', tempCart[0]);
-//   tempCart = tempCart.filter((item) => item.id !== product.id);
-//   LocalStorage.setItem('cart', tempCart);
-// };
-
 export default {
   loadCart,
   setCart,
   getCart,
   emptyCart,
-  // removeItem,
 };
